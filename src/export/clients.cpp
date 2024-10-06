bc707ee5b901fed054c77102acf4a3a3823d87c5
qubka <ushakovn@tcd.ie>
2024-10-06 17:28:27 +0100

    Add header for doxygen

--- src/export/clients.cpp
+++ src/export/clients.cpp
@@ -1,3 +1,5 @@
+#include "client.h"
+
 #include <core/player_manager.h>
 #include <core/sdk/entity/cbaseentity.h>
 #include <core/sdk/entity/cbaseplayercontroller.h>
@@ -40,12 +42,7 @@ extern "C" PLUGIN_API int GetIndexFromClient(CServerSideClient* client)
 	return -1;
 }*/
 
-/**
- * @brief Retrieves a client's authentication string (SteamID).
- *
- * @param clientIndex Index of the client whose authentication string is being retrieved.
- * @return The authentication string.
- */
+//! Retrieves a client's authentication string (SteamID).
 extern "C" PLUGIN_API void GetClientAuthId(plg::string& output, int clientIndex)
 {
 	auto pPlayer = g_PlayerManager.GetPlayerBySlot(CPlayerSlot(clientIndex - 1));
@@ -63,12 +60,7 @@ extern "C" PLUGIN_API void GetClientAuthId(plg::string& output, int clientIndex)
 	std::construct_at(&output, pSteamId->Render());
 }
 
-/**
- * @brief Returns the client's Steam account ID, a unique number identifying a given Steam account.
- *
- * @param clientIndex Index of the client.
- * @return uint64_t Steam account ID.
- */
+//! Returns the client's Steam account ID, a number uniquely identifying a given Steam account. This number is the basis for the various display SteamID forms, see the AuthIdType enum for examples.
 extern "C" PLUGIN_API uint64_t GetClientAccountId(int clientIndex)
 {
 	auto pPlayer = g_PlayerManager.GetPlayerBySlot(CPlayerSlot(clientIndex - 1));
@@ -86,12 +78,7 @@ extern "C" PLUGIN_API uint64_t GetClientAccountId(int clientIndex)
 	return pSteamId->ConvertToUint64();
 }
 
-/**
- * @brief Retrieves a client's IP address.
- *
- * @param clientIndex Index of the client.
- * @return The IP address.
- */
+//! Retrieves a client's IP address.
 extern "C" PLUGIN_API void GetClientIp(plg::string& output, int clientIndex)
 {
 	auto pPlayer = g_PlayerManager.GetPlayerBySlot(CPlayerSlot(clientIndex - 1));
@@ -103,12 +90,7 @@ extern "C" PLUGIN_API void GetClientIp(plg::string& output, int clientIndex)
 	std::construct_at(&output, pPlayer->GetIpAddress());
 }
 
-/**
- * @brief Returns the client's name.
- *
- * @param clientIndex Index of the client.
- * @return The client's name.
- */
+//! Returns the client's name.
 extern "C" PLUGIN_API void GetClientName(plg::string& output, int clientIndex)
 {
 	auto pPlayer = g_PlayerManager.GetPlayerBySlot(CPlayerSlot(clientIndex - 1));
@@ -120,12 +102,7 @@ extern "C" PLUGIN_API void GetClientName(plg::string& output, int clientIndex)
 	std::construct_at(&output, pPlayer->GetName());
 }
 
-/**
- * @brief Returns the client's connection time in seconds.
- *
- * @param clientIndex Index of the client.
- * @return float Connection time in seconds.
- */
+//! Returns the client's connection time in seconds.
 extern "C" PLUGIN_API float GetClientTime(int clientIndex)
 {
 	auto pPlayer = g_PlayerManager.GetPlayerBySlot(CPlayerSlot(clientIndex - 1));
@@ -137,12 +114,7 @@ extern "C" PLUGIN_API float GetClientTime(int clientIndex)
 	return pPlayer->GetTimeConnected();
 }
 
-/**
- * @brief Returns the client's current latency (RTT).
- *
- * @param clientIndex Index of the client.
- * @return float Latency value.
- */
+//! Returns the client's current latency (RTT), more accurate than GetAvgLatency but jittering.
 extern "C" PLUGIN_API float GetClientLatency(int clientIndex)
 {
 	auto pPlayer = g_PlayerManager.GetPlayerBySlot(CPlayerSlot(clientIndex - 1));
@@ -154,12 +126,7 @@ extern "C" PLUGIN_API float GetClientLatency(int clientIndex)
 	return pPlayer->GetLatency();
 }
 
-/**
- * @brief Returns the client's access flags.
- *
- * @param clientIndex Index of the client.
- * @return uint64 Access flags as a bitmask.
- */
+//! Returns client access flags.
 extern "C" PLUGIN_API uint64 GetUserFlagBits(int clientIndex)
 {
 	auto pPlayer = g_PlayerManager.GetPlayerBySlot(CPlayerSlot(clientIndex - 1));
@@ -171,12 +138,7 @@ extern "C" PLUGIN_API uint64 GetUserFlagBits(int clientIndex)
 	return pPlayer->GetAdminFlags();
 }
 
-/**
- * @brief Sets the access flags on a client using a bitmask.
- *
- * @param clientIndex Index of the client.
- * @param flags Bitmask representing the flags to be set.
- */
+//! Sets access flags on a client using bits instead of flags.
 extern "C" PLUGIN_API void SetUserFlagBits(int clientIndex, uint64 flags)
 {
 	auto pPlayer = g_PlayerManager.GetPlayerBySlot(CPlayerSlot(clientIndex - 1));
@@ -188,14 +150,7 @@ extern "C" PLUGIN_API void SetUserFlagBits(int clientIndex, uint64 flags)
 	pPlayer->SetAdminFlags(flags);
 }
 
-/**
- * @brief Adds access flags to a client.
- *
- * If the client is not an admin, they will be given a temporary AdminId.
- *
- * @param clientIndex Index of the client.
- * @param flags Bitmask representing the flags to be added.
- */
+//! Sets access flags on a client. If the client is not an admin, a temporary, anonymous AdminId is given.
 extern "C" PLUGIN_API void AddUserFlags(int clientIndex, uint64 flags)
 {
 	auto pPlayer = g_PlayerManager.GetPlayerBySlot(CPlayerSlot(clientIndex - 1));
@@ -207,14 +162,7 @@ extern "C" PLUGIN_API void AddUserFlags(int clientIndex, uint64 flags)
 	pPlayer->SetAdminFlags(pPlayer->GetAdminFlags() | flags);
 }
 
-/**
- * @brief Removes access flags from a client.
- *
- * If the client is not an admin, this has no effect.
- *
- * @param clientIndex Index of the client.
- * @param flags Bitmask representing the flags to be removed.
- */
+//! Removes flags from a client. If the client is not an admin, this has no effect.
 extern "C" PLUGIN_API void RemoveUserFlags(int clientIndex, uint64 flags)
 {
 	auto pPlayer = g_PlayerManager.GetPlayerBySlot(CPlayerSlot(clientIndex - 1));
@@ -226,12 +174,7 @@ extern "C" PLUGIN_API void RemoveUserFlags(int clientIndex, uint64 flags)
 	pPlayer->SetAdminFlags(pPlayer->GetAdminFlags() & ~flags);
 }
 
-/**
- * @brief Checks if a certain player has been authenticated.
- *
- * @param clientIndex Index of the client.
- * @return true if the player is authenticated, false otherwise.
- */
+//! Returns if a certain player has been authenticated.
 extern "C" PLUGIN_API bool IsClientAuthorized(int clientIndex)
 {
 	auto pPlayer = g_PlayerManager.GetPlayerBySlot(CPlayerSlot(clientIndex - 1));
@@ -243,12 +186,7 @@ extern "C" PLUGIN_API bool IsClientAuthorized(int clientIndex)
 	return pPlayer->IsAuthorized();
 }
 
-/**
- * @brief Checks if a certain player is connected.
- *
- * @param clientIndex Index of the client.
- * @return true if the player is connected, false otherwise.
- */
+//! Returns if a certain player is connected.
 extern "C" PLUGIN_API bool IsClientConnected(int clientIndex)
 {
 	auto pPlayer = g_PlayerManager.GetPlayerBySlot(CPlayerSlot(clientIndex - 1));
@@ -260,12 +198,7 @@ extern "C" PLUGIN_API bool IsClientConnected(int clientIndex)
 	return pPlayer->IsConnected();
 }
 
-/**
- * @brief Checks if a certain player has entered the game.
- *
- * @param clientIndex Index of the client.
- * @return true if the player is in the game, false otherwise.
- */
+//! Returns if a certain player has entered the game.
 extern "C" PLUGIN_API bool IsClientInGame(int clientIndex)
 {
 	auto pPlayer = g_PlayerManager.GetPlayerBySlot(CPlayerSlot(clientIndex - 1));
@@ -277,12 +210,7 @@ extern "C" PLUGIN_API bool IsClientInGame(int clientIndex)
 	return pPlayer->IsInGame();
 }
 
-/**
- * @brief Checks if a certain player is the SourceTV bot.
- *
- * @param clientIndex Index of the client.
- * @return true if the client is the SourceTV bot, false otherwise.
- */
+//! Returns if a certain player is the SourceTV bot.
 extern "C" PLUGIN_API bool IsClientSourceTV(int clientIndex)
 {
 	auto client = utils::GetController(CPlayerSlot(clientIndex - 1));
@@ -294,12 +222,7 @@ extern "C" PLUGIN_API bool IsClientSourceTV(int clientIndex)
 	return client->m_bIsHLTV();
 }
 
-/**
- * @brief Checks if the client is alive or dead.
- *
- * @param clientIndex Index of the client.
- * @return true if the client is alive, false if dead.
- */
+//! Returns if the client is alive or dead.
 extern "C" PLUGIN_API bool IsClientAlive(int clientIndex)
 {
 	auto client = utils::GetController(CPlayerSlot(clientIndex - 1));
@@ -311,12 +234,7 @@ extern "C" PLUGIN_API bool IsClientAlive(int clientIndex)
 	return client->IsAlive();
 }
 
-/**
- * @brief Checks if a certain player is a fake client.
- *
- * @param clientIndex Index of the client.
- * @return true if the client is a fake client, false otherwise.
- */
+//! Returns if a certain player is a fake client.
 extern "C" PLUGIN_API bool IsFakeClient(int clientIndex)
 {
 	auto pPlayer = g_PlayerManager.GetPlayerBySlot(CPlayerSlot(clientIndex - 1));
@@ -330,12 +248,7 @@ extern "C" PLUGIN_API bool IsFakeClient(int clientIndex)
 
 /////
 
-/**
- * @brief Retrieves a client's team index.
- *
- * @param clientIndex Index of the client.
- * @return int The team index of the client.
- */
+//! Retrieves a client's team index.
 extern "C" PLUGIN_API int GetClientTeam(int clientIndex)
 {
 	auto client = utils::GetController(CPlayerSlot(clientIndex - 1));
@@ -347,12 +260,7 @@ extern "C" PLUGIN_API int GetClientTeam(int clientIndex)
 	return client->m_iTeamNum();
 }
 
-/**
- * @brief Returns the client's health.
- *
- * @param clientIndex Index of the client.
- * @return int The health value of the client.
- */
+//! Returns the client's health.
 extern "C" PLUGIN_API int GetClientHealth(int clientIndex)
 {
 	auto client = utils::GetController(CPlayerSlot(clientIndex - 1));
@@ -364,12 +272,7 @@ extern "C" PLUGIN_API int GetClientHealth(int clientIndex)
 	return client->m_iHealth();
 }
 
-/**
- * @brief Returns the client's armor value.
- *
- * @param clientIndex Index of the client.
- * @return int The armor value of the client.
- */
+//! Returns the client's armor.
 extern "C" PLUGIN_API int GetClientArmor(int clientIndex)
 {
 	auto client = utils::GetController(CPlayerSlot(clientIndex - 1));
@@ -378,15 +281,10 @@ extern "C" PLUGIN_API int GetClientArmor(int clientIndex)
 		return 0;
 	}
 
-	return client->m_Armor();
+	return 0;//client->m_Armor();
 }
 
-/**
- * @brief Retrieves the client's origin vector.
- *
- * @param output Reference to a Vector where the client's origin will be stored.
- * @param clientIndex Index of the client.
- */
+//! Returns the client's origin vector.
 extern "C" PLUGIN_API void GetClientAbsOrigin(Vector& output, int clientIndex)
 {
 	auto client = utils::GetController(CPlayerSlot(clientIndex - 1));
@@ -398,12 +296,7 @@ extern "C" PLUGIN_API void GetClientAbsOrigin(Vector& output, int clientIndex)
 	output = client->m_CBodyComponent->m_pSceneNode->m_vecAbsOrigin();
 }
 
-/**
- * @brief Retrieves the client's position angle.
- *
- * @param output Reference to a QAngle where the client's position angle will be stored.
- * @param clientIndex Index of the client.
- */
+//! Returns the client's position angle.
 extern "C" PLUGIN_API void GetClientAbsAngles(QAngle& output, int clientIndex)
 {
 	auto client = utils::GetController(CPlayerSlot(clientIndex - 1));
@@ -415,24 +308,13 @@ extern "C" PLUGIN_API void GetClientAbsAngles(QAngle& output, int clientIndex)
 	std::construct_at(&output, client->m_CBodyComponent->m_pSceneNode->m_angRotation());
 }
 
-/**
- * @brief Processes the target string to determine if one user can target another.
- *
- * @param output Reference to a vector where the result of the targeting operation will be stored.
- * @param caller Index of the client making the target request.
- * @param target The target string specifying the player or players to be targeted.
- */
+//! Returns whether a user can target another user.
 extern "C" PLUGIN_API void ProcessTargetString(std::vector<int>& output, int caller, const plg::string& target)
 {
 	g_PlayerManager.TargetPlayerString(caller, target.c_str(), output);
 }
 
-/**
- * @brief Changes a client's team.
- *
- * @param clientIndex Index of the client.
- * @param team The team index to assign the client to.
- */
+//! Changes a client's team.
 extern "C" PLUGIN_API void ChangeClientTeam(int clientIndex, int team)
 {
 	auto client = static_cast<CCSPlayerController*>(utils::GetController(CPlayerSlot(clientIndex - 1)));
@@ -444,12 +326,7 @@ extern "C" PLUGIN_API void ChangeClientTeam(int clientIndex, int team)
 	client->ChangeTeam(team);
 }
 
-/**
- * @brief Switches the player's team.
- *
- * @param clientIndex Index of the client.
- * @param team The team index to switch the client to.
- */
+//! Switches the player's team.
 extern "C" PLUGIN_API void SwitchClientTeam(int clientIndex, int team)
 {
 	auto client = static_cast<CCSPlayerController*>(utils::GetController(CPlayerSlot(clientIndex - 1)));
@@ -461,11 +338,7 @@ extern "C" PLUGIN_API void SwitchClientTeam(int clientIndex, int team)
 	client->SwitchTeam(team);
 }
 
-/**
- * @brief Respawns a player.
- *
- * @param clientIndex Index of the client to respawn.
- */
+//! Respawns a player.
 extern "C" PLUGIN_API void RespawnClient(int clientIndex)
 {
 	auto client = utils::GetController(CPlayerSlot(clientIndex - 1));
@@ -485,13 +358,7 @@ extern "C" PLUGIN_API void RespawnClient(int clientIndex)
 	}
 }
 
-/**
- * @brief Forces a player to commit suicide.
- *
- * @param clientIndex Index of the client.
- * @param explode If true, the client will explode upon death.
- * @param force If true, the suicide will be forced.
- */
+//! Forces a player to commit suicide.
 extern "C" PLUGIN_API void ForcePlayerSuicide(int clientIndex, bool explode, bool force)
 {
 	auto client = utils::GetController(CPlayerSlot(clientIndex - 1));
@@ -503,35 +370,19 @@ extern "C" PLUGIN_API void ForcePlayerSuicide(int clientIndex, bool explode, boo
 	client->GetPawn()->CommitSuicide(explode, force);
 }
 
-/**
- * @brief Disconnects a client from the server as soon as the next frame starts.
- *
- * @param clientIndex Index of the client to be kicked.
- */
+//! Disconnects a client from the server as soon as the next frame starts.
 extern "C" PLUGIN_API void KickClient(int clientIndex)
 {
 	g_pEngineServer2->DisconnectClient(CPlayerSlot(clientIndex - 1), NETWORK_DISCONNECT_KICKED);
 }
 
-/**
- * @brief Bans a client for a specified duration.
- *
- * @param clientIndex Index of the client to be banned.
- * @param duration Duration of the ban in seconds.
- * @param kick If true, the client will be kicked immediately after being banned.
- */
+//! Bans a client.
 extern "C" PLUGIN_API void BanClient(int clientIndex, float duration, bool kick)
 {
 	g_pEngineServer2->BanClient(CPlayerSlot(clientIndex - 1), duration, kick);
 }
 
-/**
- * @brief Bans an identity (either an IP address or a Steam authentication string).
- *
- * @param steamId The Steam ID to ban.
- * @param duration Duration of the ban in seconds.
- * @param kick If true, the client will be kicked immediately after being banned.
- */
+//! Bans an identity (either an IP address or auth string).
 extern "C" PLUGIN_API void BanIdentity(uint64_t steamId, float duration, bool kick)
 {
 	g_pEngineServer2->BanClient(CSteamID(static_cast<uint64>(steamId)), duration, kick);
